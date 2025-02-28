ae8e10b6496547a7aab14ece8e73910106928bb3
Felipe Valencia <felipevalencia271@gmail.com>
2025-02-28 00:19:08 -0500

    Correcion completa

--- Calc.py
+++ Calc.py
@@ -3,27 +3,35 @@ from antlr4 import *
 from LabeledExprVisitor import LabeledExprVisitor
 from LabeledExprLexer import LabeledExprLexer
 from LabeledExprParser import LabeledExprParser
-from CommonLexerRules import CommonLexerRules
-from EvalVisitor import  Evalvisitor
-
+from EvalVisitor import Evalvisitor
 
 def main():
+    input_file = sys.argv[1] if len(sys.argv) > 1 else None
+    visitor = Evalvisitor()  # Instancia del visitor
 
-    visitor=Evalvisitor() #Instancia visitor
+    if input_file:
+        with open(input_file, 'r') as file:
+            lines = file.readlines()
+    else:
+        lines = []
+        try:
+            while True:
+                line = input().strip()
+                if line:  # Evita procesar líneas vacías
+                    lines.append(line)
+        except EOFError:
+            pass  # Captura Ctrl+D o Ctrl+Z para salir
 
-    while True:
-        expr=input("ingrese una expresion: ")
+    for line in lines:
+        input_stream = InputStream(line)
+        lexer = LabeledExprLexer(input_stream)
+        token_stream = CommonTokenStream(lexer)
+        parser = LabeledExprParser(token_stream)
+        arbol = parser.expr()  # Evaluar cada línea como una expresión individual
 
-        texto=InputStream(expr + "\n")
-        lexer=LabeledExprLexer(texto)
-        TokenStream=CommonTokenStream(lexer)
-        parser=LabeledExprParser(TokenStream)
-        arbol= parser.prog()
-
-        result=visitor.visit(arbol) #evaluar con el visitor
-
+        result = visitor.visit(arbol)  # Evaluar con el visitor
         if result is not None:
-            print("Resultado: ", result)
+            print("Resultado:", result)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
